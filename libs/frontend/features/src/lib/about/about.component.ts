0cd881c5e437bba34c04a2169e3b292951edb933
Steen010 <kjh.vansteen@student.avans.nl>
2023-11-13 23:58:34 +0100

    workend on about page

---
+++ libs/frontend/features/src/lib/about/about.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'avans-indiv-p2-about',
+  templateUrl: './about.component.html',
+  styles: [],
+})
+export class AboutComponent {}
